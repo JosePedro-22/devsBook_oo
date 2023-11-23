8e748bfe2fd6b82bee271ba5f8d2cb4b64bf78d0
Jose Pedro <jose.mesquita@seati.ma.gov.br>
2023-11-23 17:43:08 -0300

    continuando autenticacao de login

--- dao/UserDAOMysql.php
+++ dao/UserDAOMysql.php
@@ -1,10 +1,24 @@
 <?php
 
-require_once './models/User.php';
+require_once 'models/User.php';
 
 class UserDaoMysql implements UserDAO {
+    private $pdo;
+
+    public function __construct(PDO $driver){
+        $this->pdo = $driver;
+    }
+
     public function findByToken($token)
     {
-        
+        if(!empty($token)){
+            $sql = $this->pdo->prepare('SELECT * FROM users WHERE token = :token');
+            $sql->bindParam(':token',$token);
+            $sql->execute();
+
+            if($sql->rowCount() > 0) $data = $sql->fetch(PDO::FETCH_ASSOC);
+        }
+
+        return false;
     }
 }
--- models/Auth.php
+++ models/Auth.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'dao/UserDAOMysql.php';
 
 class Auth{
     private $pdo, $base;
@@ -9,7 +10,12 @@ class Auth{
     }
     public function checkToken(){
         if(!empty($_SESSION['token'])){
+            $token = $_SESSION['token'];
 
+            $userDao = new UserDaoMysql($this->pdo);
+            $user = $userDao->findByToken($token);
+
+            if($user) return $user;
         }
 
         header('location:'.$this->base.'/login;php');
